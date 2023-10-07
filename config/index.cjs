ba02912a6cbf08b5fbac72b8a9a2f7c8678e9b18
Nepoleon <37949632+NepoleonASM@users.noreply.github.com>
2023-10-07 23:43:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,28 +21,28 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'TATTA',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o1si-6W0s9xLBS9dJok2TeU4wGNQ',
+      id: 'o1si-6YXdGIBUjneRycV2QJ9vToY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'GgQ3Muhd3Oisd-lc6OvWfqiX5UiRwhJLNK5Stge_uQQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-04',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: 'zyz', year: '1998', date: '12-04',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: 'zyz', year: '1998', date: '10-16',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '10-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-10-04' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
